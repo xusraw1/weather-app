109fcb6673e368d49da680c09a9736122e76b55d
Xusrav <xusrav200329@gmail.com>
2023-04-09 19:14:32 +0500

    Change code in views.py and homepage.html, then I run server for cheking correctly work!

--- main/views.py
+++ main/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 
 # Create your views here.
 
 def home(request):
     weather = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=fce82447e26f829b9a3e17cd872aded6'
-    city = 'Tashkent'
-    res = requests.get(weather.format(city)).json()
-    city_info = {
-        'city': city,
-        'temp': res['main']['temp'],
-        'icon': res['weather'][0]['icon'],
-        'country': res['sys']['country'],
-    }
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(weather.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res['main']['temp'],
+            'icon': res['weather'][0]['icon'],
+            'country': res['sys']['country'],
+        }
+        all_cities.append(city_info)
 
     context = {
-        'info': city_info,
+        'all_info': all_cities,
     }
     return render(request, 'main/homepage.html', context)
