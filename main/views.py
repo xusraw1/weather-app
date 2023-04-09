688b5cddea459b7e9bf10188848983cea5355abd
Xusrav <xusrav200329@gmail.com>
2023-04-09 18:45:12 +0500

    Create templates dir and thier create homepage.html,then I write some code with using bootstrap, then add in views.py logic for view weather info, i get api key from using openweathermap

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
+
+def home(request):
+    weather = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=fce82447e26f829b9a3e17cd872aded6'
+    city = 'Tashkent'
+    res = requests.get(weather.format(city)).json()
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather']['icon'],
+        'country': res['sys']['country']
+    }
+
+    context = {
+        'info': city_info,
+    }
+    return render(request, 'main/homepage.html', context)
