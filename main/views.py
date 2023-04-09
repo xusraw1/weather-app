7d7a6ea6cbba982f22db50e9fc0a40fa36d20341
Xusrav <xusrav200329@gmail.com>
2023-04-09 19:28:40 +0500

    Create forms.py for cahnge button in web-site whos works searching about wether info and runserver for cheking working website

--- main/views.py
+++ main/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
 
 
 # Create your views here.
 
 def home(request):
     weather = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=fce82447e26f829b9a3e17cd872aded6'
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm
     cities = City.objects.all()
 
     all_cities = []
@@ -23,5 +29,6 @@ def home(request):
 
     context = {
         'all_info': all_cities,
+        'form': form,
     }
     return render(request, 'main/homepage.html', context)
